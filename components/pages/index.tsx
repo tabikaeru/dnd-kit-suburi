ccbe6b911a65c371f77cb99dfa0c5e387547465a
tabikaeru <moriokayuixxx@gmail.com>
2023-01-23 20:43:53 +0900

    ✨add editor

--- components/pages/index.tsx
+++ components/pages/index.tsx
@@ -1,11 +1,11 @@
 import Link from 'next/link'
 
 const Page = () => {
-  const pageList = ['sort']
+  const pageList = ['sort', 'editor']
   return (
     <div style={{ width: '100%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
       {pageList.map((listItem) => (
-        <Link key={listItem} href={`/${pageList}`}>
+        <Link key={listItem} href={`/${listItem}`}>
           {listItem}
         </Link>
       ))}
---
+++ pages/editor.tsx
@@ -0,0 +1,2 @@
+import Page from '~/components/pages/editor'
+export default Page
